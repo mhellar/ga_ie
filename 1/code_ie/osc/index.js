7fbbb773d6a897cb8f808d4a4a07881c5e2b7a2e
Mark Hellar <mark.hellar@gmail.com>
2016-03-03 18:16:50 -0800

    Day 2 Commit

--- 1/code_ie/osc/index.js
+++ 1/code_ie/osc/index.js
@@ -1,6 +1,6 @@
 var osc = require('node-osc');
 
-var oscServer = new osc.Server(8001, '10.2.0.147');
+var oscServer = new osc.Server(8337, '10.2.0.147');
 oscServer.on("message", function(msg, rinfo) {
     console.log("TUIO message:");
     console.log(msg);
