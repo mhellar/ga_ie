c526821207d73d46225ea78e6566971153af156a
mhellar <mark.hellar@gmail.com>
2017-03-28 09:36:30 -0700

    day1

--- 1/code_ie/osc/index.js
+++
@@ -1,7 +0,0 @@
-var osc = require('node-osc');
-
-var oscServer = new osc.Server(8001, '172.20.10.3');
-oscServer.on("message", function(msg, rinfo) {
-    console.log("TUIO message:");
-    console.log(msg);
-});
